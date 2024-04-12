b440c4ea6672a19ada6dc8f5cd1190a4d41bdf1c
yashwanth <ch.yashwanth777@gmail.com>
2024-04-12 19:11:17 +0530

    database added

---
+++ login_database.sql
@@ -0,0 +1,13 @@
+create table login_details(
+    username varchar(255) not null,
+    email varchar(255),
+    password text not null,
+    contact varchar(255) not null,
+    unique(username),
+    unique(contact),
+    primary key (email)
+);
+
+alter table login_details modify column password varchar(255);
+desc login_details;
+alter table login_details modify column password varchar(255) not null unique;
